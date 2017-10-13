a5c87a8b50f6b5eeb594a838ff5fb1a5174b0b89
François Poulain <fpoulain@metrodore.fr>
2017-10-13 14:04:05 +0200

    factor api call code

--- molecule/icinga/tests/test_icinga.py
+++ molecule/icinga/tests/test_icinga.py
@@ -11,46 +11,51 @@ def get_auth(File):
         re.search('password = "(.*)"', f.content_string).group(1)
     )
     
-def test_icinga_api_hosts(File, host):
+def get_master_address(host):
     inventory = yaml.load(open(host.backend.ansible_inventory))
     address = inventory['all']['hosts'][host.backend.host]['ansible_host']
+    return address
+
+def sloppy_get(url, headers={}, auth=None):
     s = requests.Session()
-    s.auth = get_auth(File)
-    s.headers.update({'Accept': 'application/json'})
+    s.auth = auth
+    s.headers.update(headers)
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    r = s.get('https://{address}:5665/v1/objects/hosts'.format(
-        address=address,
-    ), verify=False)
+    r = s.get(url, verify=False)
     r.raise_for_status()
+    return r
+
+def test_icinga_api_hosts(File, host):
+    address = get_master_address(host)
+    r = sloppy_get(
+            'https://{address}:5665/v1/objects/hosts'.format(address=address,),
+            {'Accept': 'application/json'},
+            get_auth(File),
+            )
     answer = r.json()
     assert len(answer['results']) == 1
     assert answer['results'][0]['name'] == 'icinga_host'
 
 def test_icinga_api_services (File, host):
-    inventory = yaml.load(open(host.backend.ansible_inventory))
-    address = inventory['all']['hosts'][host.backend.host]['ansible_host']
-    s = requests.Session()
-    s.auth = get_auth(File)
-    s.headers.update({'Accept': 'application/json'})
-    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    r = s.get('https://{address}:5665/v1/objects/services'.format(
-        address=address,
-    ), verify=False)
-    r.raise_for_status()
+    address = get_master_address(host)
+    r = sloppy_get(
+            'https://{address}:5665/v1/objects/services'.format(address=address,),
+            {'Accept': 'application/json'},
+            get_auth(File),
+            )
     answer = r.json()
     assert len(answer['results']) > 10
 
 def test_icingaweb2_login_screen(File, host):
-    inventory = yaml.load(open(host.backend.ansible_inventory))
-    address = inventory['all']['hosts'][host.backend.host]['ansible_host']
+    address = get_master_address(host)
     s = requests.Session()
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
     r = s.get('http://{address}/icingaweb2/authentication/login'.format(
         address=address,
-    ), verify=False)
+    ))
     cookies= dict(r.cookies)
     r = s.get('http://{address}/icingaweb2/authentication/login?_checkCookie=1'.format(
         address=address,
-    ), cookies=cookies, verify=False)
+    ), cookies=cookies)
     r.raise_for_status()
     assert 'Icinga Web 2 Login' in r.text
