a5d1809fc1f79160a1f62e90020e08c3b0f41c05
Loic Dachary <loic@dachary.org>
2017-10-15 18:42:42 +0200

    bind: focus on one bind_client_host not all hosts

--- molecule/bind/tests/test_bind.py
+++ molecule/bind/tests/test_bind.py
@@ -3,13 +3,12 @@ import time
 import testinfra
 import yaml
 
+testinfra_hosts = ['bind_client_host']
 
 def test_bind(host):
-    if host.backend.host != 'bind_client_host':
-        pytest.skip("only run on bind_client_host")
-
     inventory = yaml.load(open(host.backend.ansible_inventory))
     address = inventory['all']['hosts']['bind_host']['ansible_host']
     cmd = host.run("getent hosts ns1.test.securedrop.club")
     assert 0 == cmd.rc
-    assert address + "   ns1.test.securedrop.club" == cmd.stdout.strip()
+    assert address in cmd.stdout.strip()
+    assert "ns1.test.securedrop.club" in cmd.stdout.strip()
