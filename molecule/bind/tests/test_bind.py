e33f156d40ef25e11bf6003c79c3160ac61aa21a
Loic Dachary <loic@dachary.org>
2017-10-18 09:50:23 +0200

    bind: open port 53 udp in the firewall

--- molecule/bind/tests/test_bind.py
+++ molecule/bind/tests/test_bind.py
@@ -8,7 +8,7 @@ testinfra_hosts = ['bind_client_host']
 def test_bind(host):
     inventory = yaml.load(open(host.backend.ansible_inventory))
     address = inventory['all']['hosts']['bind_host']['ansible_host']
-    cmd = host.run("getent hosts ns1.test.securedrop.club")
+    cmd = host.run("getent hosts ns1.securedrop.club")
     assert 0 == cmd.rc
     assert address in cmd.stdout.strip()
-    assert "ns1.test.securedrop.club" in cmd.stdout.strip()
+    assert "ns1.securedrop.club" in cmd.stdout.strip()
