e8e76c257ebaba0a763716329d5302435e97ac4e
Loic Dachary <loic@dachary.org>
2017-10-22 00:07:23 +0200

    bind: move bind tests from weblate

--- molecule/bind/tests/test_bind.py
+++ molecule/bind/tests/test_bind.py
@@ -12,3 +12,18 @@ def test_bind(host):
     assert 0 == cmd.rc
     assert address in cmd.stdout.strip()
     assert "ns1.securedrop.club" in cmd.stdout.strip()
+
+def test_dmarc(host):
+    cmd = host.run("dig +short TXT _dmarc.securedrop.club")
+    assert 0 == cmd.rc
+    assert "v=DMARC1" in cmd.stdout.strip()
+
+def test_spf(host):
+    cmd = host.run("dig +short TXT securedrop.club")
+    assert 0 == cmd.rc
+    assert "v=spf1" in cmd.stdout.strip()
+
+def test_dkim(host):
+    cmd = host.run("dig +short TXT mail._domainkey.securedrop.club")
+    assert 0 == cmd.rc
+    assert "v=DKIM1" in cmd.stdout.strip()
