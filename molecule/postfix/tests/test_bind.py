d9bc6646721d3485987ce120127d832bc8fb5783
Loic Dachary <loic@dachary.org>
2017-11-02 15:09:59 +0100

    postfix: add mail related TXT records to the DNS

--- molecule/postfix/tests/test_bind.py
+++ molecule/postfix/tests/test_bind.py
@@ -1,5 +1,18 @@
 testinfra_hosts = ['postfix-client-host']
 
+
+def test_dmarc(host):
+    domain = host.run("hostname -d").stdout.strip()
+    cmd = host.run("dig +short TXT _dmarc." + domain)
+    assert 0 == cmd.rc
+    assert "v=DMARC1" in cmd.stdout.strip()
+
+def test_spf(host):
+    domain = host.run("hostname -d").stdout.strip()
+    cmd = host.run("dig +short TXT " + domain)
+    assert 0 == cmd.rc
+    assert "v=spf1" in cmd.stdout.strip()
+
 def test_dkim(host):
     domain = host.run("hostname -d").stdout.strip()
     cmd = host.run("dig +short TXT mail._domainkey." + domain)
