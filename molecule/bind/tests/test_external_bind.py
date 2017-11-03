ce0d7fd7fd98d9d981a023506a58054648da021d
Loic Dachary <loic@dachary.org>
2017-11-03 09:14:20 +0100

    bind: open port 53 to allow for zone transfer
    Fixes main/securedrop-club#12

--- molecule/bind/tests/test_external_bind.py
+++ molecule/bind/tests/test_external_bind.py
@@ -5,6 +5,11 @@ def test_bind(host):
     cmd = host.run("getent hosts ns1.{}".format(domain))
     assert 0 == cmd.rc
 
+def test_zone_transfert(host):
+    domain = host.run("hostname -d").stdout.strip()
+    cmd = host.run("dig axfr {domain} @ns1.{domain}".format(domain=domain))
+    assert 0 == cmd.rc
+
 def test_recursion(host):
     cmd = host.run("getent hosts fsf.org")
     assert 2 == cmd.rc
