47010e5f698f1e5d85b40c96c5e79bab7b5278f4
Loic Dachary <loic@dachary.org>
2017-11-19 00:48:07 +0100

    gitlab: implement with_fake_LE tests

--- molecule/gitlab/tests/test_gitlab.py
+++ molecule/gitlab/tests/test_gitlab.py
@@ -8,13 +8,20 @@ testinfra_hosts = ['gitlab-host']
 
 
 def get_address(host):
+    for vars_dir in ('group_vars/all', '../../inventory/group_vars/all'):
+        with_https_path = vars_dir + '/with_https.yml'
+        if os.path.exists(with_https_path):
+            with_https = yaml.load(open(with_https_path))
+            if with_https and with_https.get('with_https'):
+                return ('https', 'lab.' + yaml.load(
+                    open(vars_dir + '/domain.yml'))['domain'])
     inventory = yaml.load(open(host.backend.ansible_inventory))
-    address = inventory['all']['hosts']['gitlab-host']['ansible_host']
-    return address
+    return ('http', inventory['all']['hosts']['gitlab-host']['ansible_host'])
 
 
 def get_password():
-    variables = yaml.load(open('roles/gitlab/defaults/main.yml'))
+    variables = yaml.load(open(
+        '../../molecule/gitlab/roles/gitlab/defaults/main.yml'))
     return variables['gitlab_password']
 
 
@@ -22,22 +29,25 @@ def get_private_token(url):
     r = requests.post(url + '/session', data={
         'login': 'root',
         'password': get_password(),
-    })
+    }, verify='../../certs')
     r.raise_for_status()
     return r.json()['private_token']
 
 
 def recreate_test_project(url, headers):
-    r = requests.get(url + '/projects/root%2Ftestproject', headers=headers)
+    r = requests.get(url + '/projects/root%2Ftestproject',
+                     headers=headers,
+                     verify='../../certs')
     if r.status_code == requests.codes.ok:
         r = requests.delete(url + '/projects/root%2Ftestproject',
-                            headers=headers)
+                            headers=headers,
+                            verify='../../certs')
         r.raise_for_status()
     for _ in range(10):
         r = requests.post(url + '/projects', headers=headers, data={
             "name": "testproject",
             "visibility": "public",
-        })
+        }, verify='../../certs')
         time.sleep(5)
         if r.status_code == 201:
             break
@@ -47,8 +57,8 @@ def recreate_test_project(url, headers):
 
 def test_ci_runner(host, tmpdir):
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    address = get_address(host)
-    url = 'http://' + address + '/api/v3'
+    (scheme, address) = get_address(host)
+    url = scheme + '://' + address + '/api/v3'
     headers = {'PRIVATE-TOKEN': get_private_token(url)}
     recreate_test_project(url, headers)
     host.run("rm -f /tmp/SERVERS")
@@ -65,11 +75,14 @@ def test_ci_runner(host, tmpdir):
     ) > .gitlab-ci.yml
     git add .gitlab-ci.yml
     git commit -m 'test'
-    git remote add origin http://root:{password}@{address}/root/testproject.git
+    git config http.sslVerify false
+    git remote add origin \
+         {scheme}://root:{password}@{address}/root/testproject.git
     git push -u origin master
     """.format(password=get_password(),
                address=address,
-               directory=str(tmpdir)))
+               directory=str(tmpdir),
+               scheme=scheme))
     for _ in range(20):
         if (host.file('/tmp/SERVERS').exists and
                 host.file('/tmp/SERVERS').contains('gitlab-host')):
