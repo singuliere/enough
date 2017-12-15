1cafcfe687a7d8ff5732ff3420238e004f520049
François Poulain <fpoulain@metrodore.fr>
2017-12-15 16:29:14 +0100

    quiet scripts

--- molecule/demo/roles/securedrop/files/start-securedrop-demo.sh
+++ molecule/demo/roles/securedrop/files/start-securedrop-demo.sh
@@ -5,4 +5,5 @@ cd /srv/securedrop
 # sleep 1 needed for magical reasons; launcher fails whithout it
 vagrant ssh -c 'sudo start-stop-daemon -C --start --quiet --chuid vagrant:vagrant \
    --make-pidfile --pidfile /var/run/securedrop.pid --background --chdir /vagrant/securedrop \
-   --startas /bin/bash -- -c "exec /vagrant/securedrop/manage.py run > /dev/null 2>&1 " && sleep 1'
+   --startas /bin/bash -- -c "exec /vagrant/securedrop/manage.py run > /dev/null 2>&1 " && sleep 1' \
+   2>/dev/null
--- molecule/demo/roles/securedrop/files/stop-securedrop-demo.sh
+++ molecule/demo/roles/securedrop/files/stop-securedrop-demo.sh
@@ -2,4 +2,5 @@
 
 cd /srv/securedrop
 
-vagrant ssh -c 'pkill -P $(cat /var/run/securedrop.pid) && sudo rm -f /var/run/securedrop.pid'
+vagrant ssh -c 'pkill -P $(cat /var/run/securedrop.pid) && sudo rm -f /var/run/securedrop.pid' \
+   2>/dev/null
