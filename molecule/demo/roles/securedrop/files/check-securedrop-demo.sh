9704a86b8da982a631fe9fac4eebba36a75de7fd
François Poulain <fpoulain@metrodore.fr>
2017-12-15 17:50:07 +0100

    demo: add auto-rebuild script and cron

---
+++ molecule/demo/roles/securedrop/files/check-securedrop-demo.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+cd /srv/securedrop
+box_ip=$(vagrant ssh-config development | awk '$1 == "HostName" {print $2}')
+
+# test if curl didn't answered correctly
+
+if ! curl -s $box_ip:8080 2>&1 >> /dev/null
+then
+    echo should rebuild demo
+    if pgrep -f rebuild-securedrop-demo >/dev/null
+    then
+        echo already rebuilding
+        exit
+    fi
+    /usr/local/bin/rebuild-securedrop-demo.sh
+    exit
+fi
+
+if ! curl -s $box_ip:8081 2>&1 >> /dev/null
+then
+    echo should rebuild demo
+    if pgrep -f rebuild-securedrop-demo >/dev/null
+    then
+        echo already rebuilding
+        exit
+    fi
+    /usr/local/bin/rebuild-securedrop-demo.sh
+    exit
+fi
+
+# test if journalist credentials has been changed
+
+sha1="ec5b64969bc6f3d74621810bf61e697f1431ca75"
+if ! vagrant ssh -c 'sqlite3 /var/lib/securedrop/db.sqlite "select id,username,pw_salt,pw_hash,is_admin,otp_secret,is_totp,hotp_counter,created_on from journalists" | sha1sum'  2>/dev/null | grep -q $sha1
+then
+    echo should reset credentials
+    /usr/local/bin/stop-securedrop-demo.sh
+    /usr/local/bin/reset-securedrop-demo.sh
+    /usr/local/bin/start-securedrop-demo.sh
+    exit
+fi
+
+# test if upstream repo has been updated
+# if things go wrong, demo will be rebuilt at next run
+
+if git status -uno | grep -q "Votre branche est à jour avec 'origin/master'."
+then
+    echo should pull and restart
+    /usr/local/bin/stop-securedrop-demo.sh
+    git pull
+    /usr/local/bin/start-securedrop-demo.sh
+    exit
+fi
