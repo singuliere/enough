e25fef8c0ba11b32f0603675ae07030112791268
Loic Dachary <loic@dachary.org>
2018-01-23 18:41:00 +0100

    packages: fix to work in preprod
    a) Do not use docker cache when building
    
    If the test is run more than once the docker cache may become a
    problem. For instance the apt-get update run a few days ago may no
    longer be accurate because some packages are not found anymore.
    
    Run apt-get install on the same line as apt-get update so they
    belong to the same layer.
    
    Add --no-cache so that indirect dependencies of the ossec package used
    to test installation are not impacted.
    
    b) Load letsencrypt staging certificates
    
    When running in preproduction the certificates are not valid and need
    to be explicitly loaded or ignored.
    
    Fixes main/securedrop-club#52

--- molecule/packages/tests/test_packages.py
+++ molecule/packages/tests/test_packages.py
@@ -4,25 +4,16 @@ testinfra_hosts = ['packages-host']
 
 
 def test_packages(host):
+    host.ansible("file", "dest=/tmp/packages-try state=directory", check=False)
+    host.ansible("copy",
+                 "dest=/tmp/packages-try/Dockerfile "
+                 "src=tests/packages-dockerfile", check=False)
     cmd = host.run("""
-    docker image rm try-packages 2>/dev/null || true
-    rm -fr /tmp/packages-try
     set -xe
+    docker image rm packages-try 2>/dev/null || true
     flock /tmp/update-packages bash -x /srv/update-packages.sh
-    mkdir /tmp/packages-try
-    (
-       echo 'FROM ubuntu:14.04'
-       echo 'RUN apt-get update'
-       echo 'RUN apt-get install -y wget'
-       echo 'RUN wget {url}/key.asc'
-       echo 'RUN apt-key add key.asc'
-       echo 'RUN apt-get install -y software-properties-common'
-       echo 'RUN apt-add-repository {url}/develop'
-       echo 'RUN apt-get update'
-       echo 'RUN apt-get install -y securedrop-ossec-agent'
-    ) > /tmp/packages-try/Dockerfile
-
-    docker build --tag packages-try /tmp/packages-try
+    sed -i -e 's|%%url%%|{url}|g' /tmp/packages-try/Dockerfile
+    docker build --no-cache --tag packages-try /tmp/packages-try
     """.format(url=utils.get_address(host)))
     print(cmd.stdout)
     print(cmd.stderr)
