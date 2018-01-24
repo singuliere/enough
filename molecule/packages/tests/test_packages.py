5b3965380eb5bd7aabe490f7b0ef2949127d4728
Loic Dachary <loic@dachary.org>
2018-01-24 16:31:39 +0100

    packages: log package building output

--- molecule/packages/tests/test_packages.py
+++ molecule/packages/tests/test_packages.py
@@ -11,10 +11,24 @@ def test_packages(host):
     cmd = host.run("""
     set -xe
     docker image rm packages-try 2>/dev/null || true
-    flock /tmp/update-packages bash -x /srv/update-packages.sh
+    flock /tmp/update-packages \
+          bash -x /srv/update-packages.sh develop \
+          >> /var/log/update-packages.log 2>&1
     sed -i -e 's|%%url%%|{url}|g' /tmp/packages-try/Dockerfile
     docker build --no-cache --tag packages-try /tmp/packages-try
     """.format(url=utils.get_address(host)))
+    print(cmd.stdout)
+    print(cmd.stderr)
+    assert 0 == cmd.rc
+
+    cmd = host.run("""
+    set -xe
+
+    ! test -f /var/log/update-packages.log.1.gz
+    sudo /usr/sbin/logrotate --force /etc/logrotate.d/packages-logrotate
+    test -f /var/log/update-packages.log.1.gz
+    test debian = $(stat --format=%U /var/log/update-packages.log)
+    """)
     print(cmd.stdout)
     print(cmd.stderr)
     assert 0 == cmd.rc
