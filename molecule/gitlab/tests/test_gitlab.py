1d19a9d8ede01f34b5111badef3d16189dd393ba
Loic Dachary <loic@dachary.org>
2018-01-30 17:10:00 +0100

    gitlab: run the CI on another host

--- molecule/gitlab/tests/test_gitlab.py
+++ molecule/gitlab/tests/test_gitlab.py
@@ -1,5 +1,6 @@
 import urllib3
 import os
+import testinfra
 import time
 
 import gitlab_utils
@@ -8,12 +9,17 @@ testinfra_hosts = ['gitlab-host']
 
 
 def test_ci_runner(host, tmpdir):
+    lab_host = host
+    runner_host = testinfra.host.Host.get_host(
+        'ansible://runner-host',
+        ansible_inventory=host.backend.ansible_inventory)
+
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    (scheme, address) = gitlab_utils.get_address(host)
+    (scheme, address) = gitlab_utils.get_address(lab_host)
     url = scheme + '://' + address + '/api/v3'
     headers = {'PRIVATE-TOKEN': gitlab_utils.get_private_token(url)}
     gitlab_utils.recreate_test_project(url, headers, 'root', 'testproject')
-    host.run("rm -f /tmp/SERVERS")
+    runner_host.run("rm -f /tmp/SERVERS")
     os.system("""
     set -ex
     cd {directory}
@@ -38,8 +44,8 @@ def test_ci_runner(host, tmpdir):
 
     success = False
     for _ in range(40):
-        if (host.file('/srv/TEST').exists and
-                host.file('/srv/TEST').contains('OS_TENANT_NAME')):
+        if (runner_host.file('/srv/TEST').exists and
+                runner_host.file('/srv/TEST').contains('OS_TENANT_NAME')):
             success = True
             break
         time.sleep(5)
