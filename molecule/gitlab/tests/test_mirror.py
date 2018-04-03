15f094bf8c2f39f39950416bd8bdef7c5c4854d4
Loic Dachary <loic@dachary.org>
2018-04-03 22:06:53 +0200

    gitlab: upgrade to 10.5.6 with GITLAB_SHARED_RUNNERS_REGISTRATION_TOKEN
    https://gitlab.com/gitlab-org/gitlab-ce/issues/24030 is still open
    but a new environment variable makes it possible to preset
    the registration token with
    
       GITLAB_SHARED_RUNNERS_REGISTRATION_TOKEN
    
    and there no longer is a need for the API endpoint for test purposes.
    
    https://gitlab.com/gitlab-org/gitlab-ce/merge_requests/6642/diffs
    
    Fixes: main/securedrop-club#59
    
    The private token was also used for tests and is replaced by
    a resource owner password credential
    
    https://docs.gitlab.com/ce/api/oauth2.html#resource-owner-password-credentials

--- molecule/gitlab/tests/test_mirror.py
+++ molecule/gitlab/tests/test_mirror.py
@@ -16,16 +16,17 @@ def get_variables():
 def test_mirror(host):
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
     (scheme, address) = gitlab_utils.get_address(host)
-    url = scheme + '://' + address + '/api/v3'
-    headers = {'PRIVATE-TOKEN': gitlab_utils.get_private_token(url)}
+    url = scheme + '://' + address
+    headers = {'Authorization': gitlab_utils.get_token(url)}
     variables = get_variables()
+    api = url + '/api/v3'
     user = variables['mirror_bot_user']
-    if not gitlab_utils.get_user(url, headers, user=user):
-        gitlab_utils.create_user(url, headers,
+    if not gitlab_utils.get_user(api, headers, user=user):
+        gitlab_utils.create_user(api, headers,
                                  user=user,
                                  password=variables['mirror_bot_password'])
     gitlab_utils.recreate_test_project(
-        url, headers, variables['mirror_bot_user'], 'securedrop')
+        api, headers, variables['mirror_bot_user'], 'securedrop')
     cmd = host.run("crontab -l | sed -e 's/.* \* \*//' | bash -xe")
     print(cmd.stdout)
     print(cmd.stderr)
