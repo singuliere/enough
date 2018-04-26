59b7d41d810f4220fd74123dc4c617f9d55bd9a2
Loic Dachary <loic@dachary.org>
2018-04-27 00:31:39 +0200

    packages: build tags instead of branches

--- molecule/packages/tests/test_packages.py
+++ molecule/packages/tests/test_packages.py
@@ -13,7 +13,7 @@ def test_packages(host):
     cmd = packages_host.run("""
     set -xe
     flock /tmp/update-packages \
-          bash -x /srv/update-packages.sh develop \
+          bash -x /srv/update-packages.sh 0.6 \
           >> /var/log/update-packages.log 2>&1
     """)
     print(cmd.stdout)
@@ -39,7 +39,7 @@ def test_packages(host):
     echo 'Acquire::https { Verify-Peer "false"; Verify-Host "false"; }' > /etc/apt/apt.conf.d/01https
     sudo apt-get install dpkg-dev
     domain=$(hostname -d)
-    sudo apt-add-repository --enable-source http{s}://packages.$domain/develop
+    sudo apt-add-repository --enable-source http{s}://packages.$domain/0.6
     wget http{s}://packages.$domain/key.asc
     sudo apt-key add key.asc
     sudo apt-get source linux-image-4.4.115-grsec
