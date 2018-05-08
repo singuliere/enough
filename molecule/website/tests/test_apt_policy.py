a851ef8dc6578723e6f0bcddb143a87b5d33a02b
François Poulain <fpoulain@metrodore.fr>
2018-05-08 11:44:58 +0200

    fix

--- molecule/website/tests/test_apt_policy.py
+++ molecule/website/tests/test_apt_policy.py
@@ -7,6 +7,10 @@ def check_priority (host, name, priority):
 
     candidate= re.search(r'Candidate: ([^ ]+)\n', cmd.stdout).group(1)
 
+    # Nota Bene: triple stars mean that the package is installed.
+    # So de facto the test ensure that Installed == Candidate and
+    # it might fails if there is a legitimate uninstalled update.
+    # This should not occurs in a testing environment, but...
     assert ' *** {} {}'.format(candidate, priority) in cmd.stdout
 
 def test_hugo (host):
