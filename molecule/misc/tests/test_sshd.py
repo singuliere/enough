9c87004a8c441f4f70f0dc934c337906cc6cff21
Loic Dachary <loic@dachary.org>
2018-05-14 22:38:09 +0000

    Revert "Merge branch 'fix_90' into 'master'"
    This reverts merge request !157

--- molecule/misc/tests/test_sshd.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-def test_apt(host):
-    domain = host.run("hostname -d").stdout.strip()
-    cmd = host.run("ssh-keyscan debian-host.{}".format(domain))
-    assert 0 == cmd.rc
-    assert 'ecdsa' not in cmd.stdout
-    assert 'ssh-rsa' in cmd.stdout
-    assert 'ssh-ed25519' in cmd.stdout
