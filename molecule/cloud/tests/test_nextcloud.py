f64c81403eaecca01d1701b98853fb1094e27e48
Loic Dachary <loic@dachary.org>
2018-05-17 15:31:43 +0200

    cloud: add .onion URL to trusted_domains

--- molecule/cloud/tests/test_nextcloud.py
+++ molecule/cloud/tests/test_nextcloud.py
@@ -6,7 +6,7 @@ def test_nextcloud(host):
     d=/dev/sdb
     test -e /dev/sdb || d=/dev/vda
     mount | grep $d | grep /var/lib/docker
-    curl --silent http://127.0.0.1/login | grep --quiet -i 'Forgot pass'
+    curl --silent http://127.0.0.1/login | grep --quiet 'Forgot pass'
     """)
     print(cmd.stdout)
     print(cmd.stderr)
@@ -14,14 +14,9 @@ def test_nextcloud(host):
 
 def test_nextcloud_via_tor(host):
     cmd = host.run("""
-    set -x
-    sudo apt-get install -y torsocks
-    for d in 2 4 8 16 32 64 128 256 512 ; do
-      hostname=$(sudo cat /var/lib/tor/services/cloud/hostname)
-      torsocks curl --silent http://$hostname/ | grep --quiet -i nextcloud && break
-      sleep $d
-    done
-    torify curl --silent http://$hostname/ | grep --quiet -i nextcloud
+    set -xe
+    hostname=$(sudo cat /var/lib/tor/services/cloud/hostname)
+    torsocks curl --silent http://$hostname/login | grep --quiet 'Forgot pass'
     """)
     print(cmd.stdout)
     print(cmd.stderr)
