14cd13a232fb69ebc48dd1fafa6789ef44c00a6d
François Poulain <fpoulain@metrodore.fr>
2018-05-18 10:56:31 +0200

    add test for 404 on demo

--- molecule/demo/tests/test_nginx_demo.py
+++ molecule/demo/tests/test_nginx_demo.py
@@ -36,3 +36,14 @@ def test_journalist (host):
     r = s.get('http://{}/login'.format(get_demo_address (host)), timeout=5)
     r.raise_for_status()
     assert 'Login to access the journalist interface' in r.text
+
+def test_404 (host):
+    demo_utils.check_demo(host)
+    domain = host.run("hostname -d").stdout.strip()
+    s = requests.Session()
+    # we only test 404 on landing page because
+    # - on journalist we need to login before seeing it
+    # - on source it is already served by securedrop
+    r = s.get('http://{}/bling'.format(domain), timeout=5)
+    assert r.status_code == requests.codes.not_found
+    assert 'forum.securedrop.club'
