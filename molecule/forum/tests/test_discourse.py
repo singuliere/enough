8b9c7a85fa4d6db4c937557984256f0d8d696490
Loic Dachary <loic@dachary.org>
2018-07-23 22:40:14 +0200

    forum: do not use a separate volume for docker
    Because the data resides in /var/discourse and the docker directory is
    not going to grow. And since we have 20GB total, there is plenty of
    room for attachments. The SecureDrop forum used less than 1GB in one year.

--- molecule/forum/tests/test_discourse.py
+++ molecule/forum/tests/test_discourse.py
@@ -3,9 +3,6 @@ testinfra_hosts = ['forum-host']
 def test_discourse(host):
     cmd = host.run("""
     set -xe
-    d=/dev/sdb
-    test -e /dev/sdb || d=/dev/vda
-    mount | grep $d | grep /var/lib/docker
     curl --silent https://forum.$(hostname -d) | grep --quiet 'Congratulations, you installed Discourse!'
     """)
     assert 0 == cmd.rc
