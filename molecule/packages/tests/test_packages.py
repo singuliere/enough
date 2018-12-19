d48fc49ac46f3580e6322b569d77a9aca36b9a1a
Loic Dachary <loic@dachary.org>
2018-12-19 16:54:20 +0100

    packages: using certbot instead of https-portal

--- molecule/packages/tests/test_packages.py
+++ molecule/packages/tests/test_packages.py
@@ -7,6 +7,7 @@ def test_packages(host):
     set -xe
 
     ! test -f /var/log/update-packages.log.1.gz
+    echo >> /var/log/update-packages.log
     sudo /usr/sbin/logrotate --force /etc/logrotate.d/packages-logrotate
     test -f /var/log/update-packages.log.1.gz
     test debian = $(stat --format=%U /var/log/update-packages.log)
@@ -16,13 +17,7 @@ def test_packages(host):
     assert 0 == cmd.rc
 
 def test_displayed_packages(host):
-    webpage= host.run("""
-    domain=$(hostname -d)
-    curl -s -m 5 http{s}://packages.$domain
-    """.format(s=('s' if try_if_letsencrypt(host) else ''))
-    assert 0 == webpage.rc
+    with host.sudo():
+        host.run("apt-get install -y curl")
 
-    for url in re.findall(r'https?://[^\s"><]+', webpage.stdout):
-        print(url)
-        cmd= host.run("curl -s -I -m 5 {}")
-        assert 'HTTP/2 200' in cmd.stdout or 'HTTP/2 301' in cmd.stdout
+    assert host.run("curl -s -m 5 https://packages.$(hostname -d)").rc == 0
