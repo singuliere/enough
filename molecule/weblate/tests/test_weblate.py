874ae64f27b9354fb6964a7b6342ef452186882a
Loic Dachary <loic@dachary.org>
2018-12-20 23:49:33 +0100

    weblate: replace https-portal with certbot

--- molecule/weblate/tests/test_weblate.py
+++ molecule/weblate/tests/test_weblate.py
@@ -4,22 +4,17 @@ import requests
 import pytest
 import yaml
 
-def get_weblate_address(host):
-    inventory = yaml.load(open(host.backend.ansible_inventory))
-    address = inventory['all']['hosts']['weblate-host']['ansible_host']
-    return address
+def get_address():
+    vars_dir = '../../inventory/group_vars/all'
+    return 'https://weblate.' + yaml.load(
+        open(vars_dir + '/domain.yml'))['domain']
 
-def test_weblate (host):
-    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+def test_weblate ():
     # weblate freshly recreated may take few mins to be operationnal
+    url = get_address()
     for i in range (60, 0, -1):
-        try:
-            s = requests.Session()
-            r = s.get('http://{}'.format(get_weblate_address (host)), timeout=5)
-            if r.status_code == requests.codes.ok:
-                break
-        except Exception as e:
-            if i == 1:
-                raise e
-            time.sleep(5)
+        r = requests.get(url, timeout=5, verify='../../certs')
+        if r.status_code == requests.codes.ok:
+            break
+        time.sleep(5)
     assert 'Weblate' in r.text
