3122a718eccee5c55410698c4836dafa04ed39f2
Loic Dachary <loic@dachary.org>
2018-12-25 15:33:32 +0100

    certbot: remove because it is replaced by letsencrypt-nginx

--- molecule/certbot/tests/test_fake_le.py
+++
@@ -1,7 +0,0 @@
-testinfra_hosts = ['certbot-host']
-
-def test_certs(host):
-    with host.sudo():
-        host.run("apt-get install -y curl")
-    assert host.run("curl -m 5 -I https://certbot-host.$(hostname -d)").rc == 0
-    assert host.run("curl -m 5 -I https://bind-host.$(hostname -d)").rc == 0
