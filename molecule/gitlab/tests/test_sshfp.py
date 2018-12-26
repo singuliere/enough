09785f1cfca9582dc0b35cbdcb96f26829f90c10
Loic Dachary <loic@dachary.org>
2018-12-26 22:14:25 +0100

    gitlab: verify lab has a SSHFP record

---
+++ molecule/gitlab/tests/test_sshfp.py
@@ -0,0 +1,16 @@
+testinfra_hosts = [ 'bind-host' ]
+
+def test_sshfp(host):
+    domain = host.run('hostname -d').stdout.strip()
+    #
+    # expected side effect of
+    #
+    #     - role: install_ssh_records
+    #       vars:
+    #         install_ssh_records_host: lab
+    #
+    cmd = host.run('dig sshfp +noall +answer @ns1.{domain} lab.{domain}'.format(
+        domain=domain))
+    assert cmd.rc == 0
+    assert 'IN\tSSHFP' in cmd.stdout
+    assert 'lab.' + domain in cmd.stdout
