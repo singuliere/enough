757ed4c680de7d2a3fbc86bea6f6f83a0cb380fe
Loic Dachary <loic@dachary.org>
2018-12-29 01:23:33 +0100

    nsupdate: get keys stored in the nsupdate directory
    so there can be users that are able to update the DNS for test
    purposes but are not allowed to ssh to any machine

---
+++ molecule/bind/tests/test_nsupdate.py
@@ -0,0 +1,7 @@
+testinfra_hosts = ['bind-host']
+
+def test_nsupdate_ssh_keys(host):
+    with host.sudo():
+        # X6yEpyb0O1DoPISER4tgxIb is
+        # authorized_keys/roles/authorized_keys/files/nsupdate/singuliere.pub
+        assert host.file('/home/subdomain/.ssh/authorized_keys').contains('X6yEpyb0O1DoPISER4tgxIb')
