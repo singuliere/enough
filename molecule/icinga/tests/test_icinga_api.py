e988323483f30f5282c449d291f1fb266e232400
singuliere <singuliere@autistici.org>
2018-12-31 16:47:44 +0100

    icinga: refactor sloppy_get into get_api_session

--- molecule/icinga/tests/test_icinga_api.py
+++ molecule/icinga/tests/test_icinga_api.py
@@ -6,6 +6,8 @@ import yaml
 
 testinfra_hosts = ['icinga-host']
 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
 def get_auth(host):
     host= host.get_host('ansible://icinga-host', ansible_inventory=host.backend.ansible_inventory)
     with host.sudo():
@@ -20,33 +22,30 @@ def get_address():
     return 'icinga.' + yaml.load(
         open(vars_dir + '/domain.yml'))['domain']
 
-def sloppy_get(url, headers={}, auth=None):
+def get_api_session(host):
     s = requests.Session()
-    s.auth = auth
-    s.headers.update(headers)
-    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    r = s.get(url, verify=False, timeout=5)
-    r.raise_for_status()
-    return r
+    s.auth = get_auth(host)
+    s.headers.update({'Accept': 'application/json'})
+    s.verify = False
+    s.timeout = 5
+    return s
 
 def test_icinga_api_hosts(host):
     address = get_address()
-    r = sloppy_get(
-            'https://{address}:5665/v1/objects/hosts'.format(address=address,),
-            {'Accept': 'application/json'},
-            get_auth(host),
-            )
+    r = get_api_session(host).get(
+        'https://{address}:5665/v1/objects/hosts'.format(address=address)
+    )
+    r.raise_for_status()
     answer = r.json()
     assert len(answer['results']) == 4
     assert set([h['name'] for h in answer['results']]) == set(['bind-host', 'icinga-host', 'monitoring-client-host', 'monitoring-client2-host'])
 
-def test_icinga_api_services (host):
+def test_icinga_api_services(host):
     address = get_address()
-    r = sloppy_get(
-            'https://{address}:5665/v1/objects/services'.format(address=address,),
-            {'Accept': 'application/json'},
-            get_auth(host),
+    r = get_api_session(host).get(
+            'https://{address}:5665/v1/objects/services'.format(address=address),
             )
+    r.raise_for_status()
     answer = r.json()
     assert len(answer['results']) > 40
     assert host.backend.host != "monitoring-client-host" or len([s for s in answer['results'] if 'monitoring-client-host!Secure Drop Forum' == s['name']]) == 1
