7504215677d834a95c12343e34ec269797566a1d
singuliere <singuliere@autistici.org>
2019-01-08 18:16:58 +0100

    wazuh: add IDS manager and agents
    Fixes: main/infrastructure#63

---
+++ molecule/wazuh/tests/test_icinga.py
@@ -0,0 +1,12 @@
+from tests.icinga_helper import IcingaHelper
+
+testinfra_hosts = ['icinga-host']
+
+class TestChecks(IcingaHelper):
+
+    def test_host(self):
+        r = self.get_client().objects.get('Host', 'wazuh-host')
+        assert r['attrs']['name'] == 'wazuh-host'
+
+    def test_service(self, host):
+        assert self.is_service_ok('wazuh-host!ping4')
---
+++ molecule/wazuh/tests/test_postfix.py
@@ -0,0 +1,28 @@
+import pytest
+import time
+import testinfra
+from tests import retry
+
+testinfra_hosts = ['wazuh-host']
+
+def test_wazuh_send_mail(host):
+
+    wazuh_host = host
+    postfix_host = testinfra.host.Host.get_host(
+        'ansible://postfix-host',
+        ansible_inventory=host.backend.ansible_inventory)
+
+    with wazuh_host.sudo():
+        def wait_for_boot_mail():
+            assert wazuh_host.run("""
+            grep -q -r 'ossec: Ossec started.' /var/spool/postfix/deferred
+            """).rc == 0
+        wait_for_boot_mail()
+
+    check = ("grep -q 'proto=ESMTP helo=<wazuh-host' "
+             "/var/log/mail.log")
+
+    @retry.retry(AssertionError, tries=8)
+    def wait_for_mail():
+        assert postfix_host.run(check).rc == 0
+    wait_for_mail()
