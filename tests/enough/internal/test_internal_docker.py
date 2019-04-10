7b53ffa952892162bc9b6472e1aeb1367de35818
singuliere <singuliere@autistici.org>
2019-04-11 00:47:37 +0200

    enough: rename 'build enough image' to 'build image'

--- tests/enough/internal/test_internal_docker.py
+++ tests/enough/internal/test_internal_docker.py
@@ -5,7 +5,7 @@ from io import StringIO
 
 
 def test_enough_docker_image(docker_name):
-    assert main(['--debug', 'build', 'enough', 'image', '--name', docker_name]) == 0
+    assert main(['--debug', 'build', 'image', '--name', docker_name]) == 0
     image_name = Docker(docker_name).get_image_name_with_version(suffix=None)
     out = StringIO()
     sh.docker('image', 'ls', '--filter', 'reference=' + image_name,
@@ -20,7 +20,7 @@ def test_enough_docker_image(docker_name):
 
 
 def test_enough_docker_service(docker_name, docker_options):
-    assert main(['build', 'enough', 'image', '--name', docker_name]) == 0
+    assert main(['build', 'image', '--name', docker_name]) == 0
     assert main(['--debug', 'create', 'service', '--name', docker_name] + docker_options) == 0
     for name in sh.docker.ps('--format', '{{ .Names }}', _iter=True):
         if docker_name in name:
