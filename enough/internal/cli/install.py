c757e6c5f99bacc1fa7b24f1b60d9ccc11c7cf73
singuliere <singuliere@autistici.org>
2019-04-13 11:07:44 +0200

    enough: install can be used for systemd, script & bash functions

--- enough/internal/cli/install.py
+++ enough/internal/cli/install.py
@@ -10,7 +10,38 @@ class InstallScript(Command):
 
     log = logging.getLogger(__name__)
 
-    def take_action(self, parsed_args):
+    def get_parser(self, prog_name):
+        parser = super(InstallScript, self).get_parser(prog_name)
+        parser.add_argument('--script', action='store_true')
+        parser.add_argument('--service', action='store_true')
+        parser.add_argument('--no-version', action='store_true')
+        return parser
+
+    def version(self, parsed_args):
+        if parsed_args.no_version:
+            return ''
+        else:
+            return f':{__version__}'
+
+    def script(self, parsed_args):
         i = os.path.join(os.path.dirname(__file__), '../data/install.sh')
         content = open(i).read()
-        print(content.replace('%version%', __version__))
+        print(content.replace('%version%', self.version(parsed_args)))
+
+    def service(self, parsed_args):
+        i = os.path.join(os.path.dirname(__file__), '../../api/data/enough.service')
+        content = open(i).read()
+        print(content.replace('%version%', self.version(parsed_args)))
+
+    def function(self, parsed_args):
+        print('function enough() {')
+        self.script(parsed_args)
+        print('}')
+
+    def take_action(self, parsed_args):
+        if parsed_args.script:
+            self.script(parsed_args)
+        elif parsed_args.service:
+            self.service(parsed_args)
+        else:
+            self.function(parsed_args)
