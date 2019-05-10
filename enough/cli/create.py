7543d5f6d7d8c9c9cca8ad97b832beda1d453809
singuliere <singuliere@autistici.org>
2019-05-11 00:54:11 +0200

    enough: shorter python super() syntax

--- enough/cli/create.py
+++ enough/cli/create.py
@@ -5,7 +5,7 @@ class Create(Command):
     "Create"
 
     def get_parser(self, prog_name):
-        parser = super(Create, self).get_parser(prog_name)
+        parser = super().get_parser(prog_name)
         return parser
 
     def take_action(self, parsed_args):
