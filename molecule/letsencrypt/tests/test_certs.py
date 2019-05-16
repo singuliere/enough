c72bcec32f91332b2a2890be711ec1f79e12d25e
singuliere <singuliere@autistici.org>
2019-05-16 15:34:32 +0200

    letsencrypt*: deprecate

--- molecule/letsencrypt/tests/test_certs.py
+++
@@ -1,8 +0,0 @@
-def test_certs(host):
-    for f in ('fakeleintermediatex1.pem', 'fakelerootx1.pem'):
-        path = '/etc/ssl/certs/' + f
-        if host.backend.host == "debian-host":
-            expected = True  # the certs are expected to be installed
-        elif host.backend.host == "bind-host":
-            expected = False  # the certs are expected be installed and then removed
-        assert host.exists(path) == expected
